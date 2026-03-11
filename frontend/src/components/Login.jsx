4070ca8e211a93780200f953acb1d31beff29136
Nikhil Kirodiwal <nikhilkirodiwal111@gmail.com>
2026-03-11 11:43:20 +0530

    login fix

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -114,7 +114,8 @@ export default function Login() {
             >
               Login to Admin Panel
             </button>
-            <p className="text-center">demo: super@admin.com / admin</p>
+            <p className="text-center">admin demo: super@admin.com / admin</p>
+            <p className="text-center">school demo: super@admin.com / admin</p>
           </form>
         </div>
       </div>
